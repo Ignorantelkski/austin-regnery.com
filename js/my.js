5618649ff6fa5f3fa70fdc6ce849cfda41e4fc4d
Austin C Regnery <austin.c.regnery@boeing.com>
2018-12-14 15:11:10 -0500

    removed content and animation

--- js/my.js
+++ js/my.js
@@ -1,11 +1,19 @@
 $(window).scroll(function () {
     if ($(this).scrollTop() > 0) {
-        $('.hero-pic').fadeIn();
-    } else {
-        $('.hero-pic').fadeOut();
+    //     $('.hero-pic').fadeIn();
+    // } else {
+    //     $('.hero-pic').fadeOut();
+    // }
+    // $('.hero-pic').attr("style", "animation: fadein 3s");
+    // } else {
+    // $('.hero-pic').attr("style", "animation: fadeout 3s");
     }
 });
 
 $('.trigger').click(function() {
   $('.about-me, .home-page').toggleClass('hide');
+});
+
+$(function() {
+    $('body').removeClass('fade-out');
 });
