ff1f43b80d45e744f178a02c4b09e14969e51476
Austin C Regnery <austin.c.regnery@boeing.com>
2019-01-31 17:00:15 -0500

    complete redesign

--- js/my.js
+++ js/my.js
@@ -1,14 +1,3 @@
-
 $('.about-trigger').click(function() {
-  $('.about-me, .home-page').toggleClass('hide');
-  $('body').toggleClass('border');
-});
-
-$('.case-1-trigger').click(function() {
-  $('.shield, .home-page').toggleClass('hide');
-  $('body').toggleClass('border');
-});
-
-$(function() {
-    $('body').removeClass('fade-out');
-});
+    window.location.hash = "jump";
+  });
