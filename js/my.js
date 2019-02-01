427787542a0be85e19b9d650171d4abf0cec54cd
Austin C Regnery <austin.c.regnery@boeing.com>
2019-02-01 16:13:30 -0500

    added smooth scroll for some browsers, scroll to top link, and favicon

--- js/my.js
+++ js/my.js
@@ -1,3 +1,8 @@
+//for cross-browser support
 $('.about-trigger').click(function() {
     window.location.hash = "jump";
   });
+
+$('.scroll-to-top-trigger').click(function() {
+  jQuery('html,body').animate({scrollTop:0},0);
+});
